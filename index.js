53e86b1392ead4cbb0c0aae4930f57ec3b2f3922
Julia Wey-Burgoyne <juliawey@gmail.com>
2023-12-12 10:16:57 -0500

    Adds Mario List & Images

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-
+document.addEventListener("DOMContentLoaded", function fetchData(){
+    fetch('https://www.amiiboapi.com/api/amiibo/?name=mario')
+        .then(res => res.json())
+        .then(data => displayCharacters(data.amiibo))
+        .catch(error => console.error('Error:', error));
+});
+    
+function displayCharacters(amiiboArray) {
+        const marioList = document.getElementById('marioList');
+        marioList.innerHTML = '';
+        
+        amiiboArray.forEach(item => {
+            const listItem = document.createElement('li');
+            listItem.textContent = `SERIES: ${item.amiiboSeries}, CHARACTER: ${item.character}`;
+            const imgElement = document.createElement('img');
+            imgElement.src = item.image;
+            imgElement.alt = `${item.character} Amiibo`;
+                
+            listItem.appendChild(imgElement);
+            marioList.appendChild(listItem);
+            });
+        }
