3164f2d6fc1eddb436e272cce5af0d1b3a936612
Julia Wey-Burgoyne <juliawey@gmail.com>
2023-12-15 11:48:45 -0500

    Adds formating and text descriptions

--- index.js
+++ index.js
@@ -4,25 +4,48 @@ document.addEventListener("DOMContentLoaded", function fetchData(){
         .then(data => displayCharacters(data.amiibo))
         .catch(error => console.error('Error:', error));
 });
-    
-function displayCharacters(amiiboArray) {
+    function displayCharacters(amiiboArray) {
         const marioList = document.getElementById('marioList');
         marioList.innerHTML = '';
+
+        amiiboArray.forEach (item => {
+            let card = document.createElement('li')
+            card.className = 'card'
+            card.innerHTML = `
+                <img src ="${item.image}" alt="${item.character} Amiibo">
+                <div class = "content">
+                    <h4>${item.name}</h4>
+                    <p>CHARACTER: ${item.character}</p>
+                    <p>AMIIBO SERIES: ${item.amiiboSeries}</p>
+                    <p>GAME SERIES: ${item.gameSeries}</p>
+                </div>
+            `;
+        marioList.appendChild(card);
+ });
+}
+
+
+ 
+
+
+// function displayCharacters(amiiboArray) {
+//         const marioList = document.getElementById('marioList');
+//         marioList.innerHTML = '';
         
-        amiiboArray.forEach(item => {
-            const listItem = document.createElement('li');
-            const imgElement = document.createElement('img');
-            imgElement.src = item.image;
-            imgElement.alt = `${item.character} Amiibo`;
+//         amiiboArray.forEach(item => {
+//             const listItem = document.createElement('li');
+//             const imgElement = document.createElement('img');
+//             imgElement.src = item.image;
+//             imgElement.alt = `${item.character} Amiibo`;
                 
-            listItem.appendChild(imgElement);
+//             listItem.appendChild(imgElement);
 
-            const infoDiv = document.createElement('div');
-            infoDiv.className = 'infoDiv';
-            infoDiv.textContent = `SERIES: ${item.amiiboSeries}, CHARACTER: ${item.character}`; 
+//             const infoDiv = document.createElement('div');
+//             infoDiv.className = 'infoDiv';
+//             infoDiv.textContent = `SERIES: ${item.amiiboSeries}, CHARACTER: ${item.character}`; 
 
-            listItem.appendChild(infoDiv);
+//             listItem.appendChild(infoDiv);
     
-            marioList.appendChild(listItem);
-            });
-        }
+//             marioList.appendChild(listItem);
+//             });
+//         }
