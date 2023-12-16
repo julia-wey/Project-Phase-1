e412cc8e564cae70792eb3840ff625a0b329c891
Julia Wey-Burgoyne <juliawey@gmail.com>
2023-12-16 09:51:37 -0500

    Adds heart click event

--- index.js
+++ index.js
@@ -18,8 +18,21 @@ document.addEventListener("DOMContentLoaded", function fetchData(){
                     <p>CHARACTER: ${item.character}</p>
                     <p>AMIIBO SERIES: ${item.amiiboSeries}</p>
                     <p>GAME SERIES: ${item.gameSeries}</p>
+                    <span class="heart-text">Click the heart if you like this amiibo. </span>
+                    <span class="heart" data-id="{item.id}" onclick="toggleHeart(this)" style="cursor: pointer; color: #ccc;">&#10084;</span>
                 </div>
             `;
         marioList.appendChild(card);
  });
+}
+
+function toggleHeart(icon) {
+    const amiiboId = icon.getAttribute('data-id');
+    const isFavorite = icon.style.color === 'red';
+
+    if (isFavorite) {
+        icon.style.color = '#ccc';
+    } else {
+        icon.style.color = 'red';
+    }
 }
