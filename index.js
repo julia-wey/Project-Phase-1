5fbc4b912d86b16c6bdbc6abee636927c42ccca3
Julia Wey-Burgoyne <juliawey@gmail.com>
2023-12-16 11:52:14 -0500

    Adds Mouse Over & Out events

--- index.js
+++ index.js
@@ -12,18 +12,49 @@ document.addEventListener("DOMContentLoaded", function fetchData(){
             let card = document.createElement('li')
             card.className = 'card'
             card.innerHTML = `
-                <img src ="${item.image}" alt="${item.character} Amiibo">
-                <div class = "content">
-                    <h4>${item.name}</h4>
-                    <p>CHARACTER: ${item.character}</p>
-                    <p>AMIIBO SERIES: ${item.amiiboSeries}</p>
-                    <p>GAME SERIES: ${item.gameSeries}</p>
-                    <span class="heart-text">Click the heart if you like this amiibo. </span>
-                    <span class="heart" data-id="{item.id}" onclick="toggleHeart(this)" style="cursor: pointer; color: #ccc;">&#10084;</span>
+                <div class="card-info">
+                    <h2>${item.name}</h2>
+                    <div class="image-container">
+                        <img src ="${item.image}" alt="${item.character} Amiibo">
+                    </div>
+                    <div class="info-container">
+                        <div class = "content">
+                            <div class="info-box">
+                                <p>CHARACTER: ${item.character}</p>
+                                <p>AMIIBO SERIES: ${item.amiiboSeries}</p>
+                                <p>GAME SERIES: ${item.gameSeries}</p>
+                            </div>
+                        <div class="info-overlay" style="display: none;"></div>
+                            <br></br>
+                        <span class="heart-text">Click the heart if you would like this amiibo. </span>
+                        <span class="heart" data-id="{item.id}" onclick="toggleHeart(this)" style="cursor: pointer; color: #ccc;">&#10084;</span>
+                    </div>
                 </div>
+            </div>
             `;
+            card.addEventListener('mouseover', function () {
+                displayInfo(card, item.type);
+            });
+            card.addEventListener('mouseout', function () {
+                hideInfo(card);
+            });
         marioList.appendChild(card);
  });
+}
+
+function displayInfo(card, type) {
+    const infoOverlay = card.querySelector('.info-overlay');
+    infoOverlay.style.display = 'block';
+    infoOverlay.innerHTML = `
+        <div class="info-box">   
+            <p>TYPE: ${type}</p>
+        </div>
+    `;
+}
+    
+function hideInfo(card) {
+    const infoOverlay = card.querySelector('.info-overlay');
+    infoOverlay.style.display = 'none';
 }
 
 function toggleHeart(icon) {
